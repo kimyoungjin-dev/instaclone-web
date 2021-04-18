d6bc0a8133368730af38fa4db6ef84ec01ce2f99
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-18 20:58:38 +0900

    How to apply styled-component with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { ourTheme } from "./styles";
+
+interface IContainerProps {}
+
+const Container = styled.div<IContainerProps>`
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.fontColor};
+`;
 
 const App = () => {
-  return <div></div>;
+  return <ThemeProvider theme={ourTheme}></ThemeProvider>;
 };
 
 export default App;
