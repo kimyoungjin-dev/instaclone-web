f2295552f92f789445703f22c5717ba7b9cf25f0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 01:48:37 +0900

    Update styles.ts
    Set the initial value by applying GlobalStyles
    
    Implement dark mode and light mode. These two types are defaultTheme

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,35 @@
-import { DefaultTheme } from "styled-components";
+import { createGlobalStyle, DefaultTheme } from "styled-components";
+import reset from "styled-reset";
 
-export const ourTheme: DefaultTheme = {
+export const dark: DefaultTheme = {
   bgColor: "black",
   fontColor: "white",
 };
+
+export const light: DefaultTheme = {
+  bgColor: "white",
+  fontColor: "black",
+};
+
+export const GlobalStyles = createGlobalStyle`
+    ${reset};
+
+    *{
+        box-sizing:border-box;
+        padding:0;
+        margin:0;
+    }
+
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    input{
+        all:unset;
+    }
+`;
