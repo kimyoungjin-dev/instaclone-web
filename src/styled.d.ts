9166173c1d977c4cf7503102303cf3f1ab40ffff
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 17:31:47 +0900

    Because accent color variable and borderColor are often used, they are designated as theme.

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,9 +1,10 @@
 import "styled-components";
 
-//styled-components의 타입 확장
 declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
     fontColor: string;
+    accent: string;
+    borderColor: string;
   }
 }
