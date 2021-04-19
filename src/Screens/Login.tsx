6e2ae4a086b5ac3cdc08bae7737de6ea54151b56
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 17:34:21 +0900

    Code organization: Separating reusable codes into separate components.

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -1,20 +1,112 @@
-import React from "react";
-import { darkModeVar } from "../Components/Apollo";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import AuthLayout from "../Components/Auth/AuthLayout";
+import Button from "../Components/Auth/Button";
+import { BaseBox } from "../Components/shared";
+import routes from "../routes";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.fontColor};
+const TopBox = styled(BaseBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+`;
+
+const Form = styled.form`
+  margin-top: 35px;
+  width: 100%;
+  display: flex;
+  justify-items: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border-radius: 3px;
+  padding: 7px;
+  background-color: #fafafa;
+  border: 0.5px solid ${(props) => props.theme.borderColor};
+  margin-top: 5px;
+  box-sizing: border-box;
+  &::placeholder {
+    font-size: 12px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 60px;
+  font-family: "Architects Daughter", cursive;
+`;
+
+const BottomBox = styled(BaseBox)`
+  padding: 20px 0px;
+  text-align: center;
+  a {
+    font-weight: 600;
+    margin-left: 5px;
+    color: ${(props) => props.theme.accent};
+  }
+`;
+
+const Separator = styled.div`
+  margin: 20px 0px 30px 0px;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  div {
+    width: 100%;
+    height: 1px;
+    background-color: ${(props) => props.theme.borderColor};
+  }
+  span {
+    margin: 0px 10px;
+    font-weight: 600;
+    color: #8e8e8e;
+  }
+`;
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+  }
 `;
 
 const Login = () => {
   return (
-    <Container>
-      <h1>Login</h1>
-      <button onClick={() => darkModeVar(true)}>다크모드</button>
-      <button onClick={() => darkModeVar(false)}>화이트모드</button>
-    </Container>
+    <AuthLayout>
+      <TopBox>
+        <div>
+          <Title>instagram</Title>
+        </div>
+        <Form>
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Log in" />
+        </Form>
+        <Separator>
+          <div></div>
+          <span>Or</span>
+          <div></div>
+        </Separator>
+        <FacebookLogin>
+          <FontAwesomeIcon icon={faFacebookSquare} />
+          <span>Log in with Facebook</span>
+        </FacebookLogin>
+      </TopBox>
+      <BottomBox>
+        <span>Don't have an account?</span>
+        <Link to={routes.signUp}>Sign up</Link>
+      </BottomBox>
+    </AuthLayout>
   );
 };
-
 export default Login;
