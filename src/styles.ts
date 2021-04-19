5bf6961df7be1beff7421ffc7bdc5fce6f028fc6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 17:32:25 +0900

    Update styles.ts
    Add a borderColor accent to the DefaultTheme.
    
    GlobalStyles css modification

--- src/styles.ts
+++ src/styles.ts
@@ -4,15 +4,24 @@ import reset from "styled-reset";
 export const dark: DefaultTheme = {
   bgColor: "black",
   fontColor: "white",
+  accent: "#0095f6",
+  borderColor: "rgb(219,219,219)",
 };
 
 export const light: DefaultTheme = {
   bgColor: "white",
   fontColor: "black",
+  accent: "#0095f6",
+  borderColor: "rgb(219,219,219)",
 };
 
 export const GlobalStyles = createGlobalStyle`
     ${reset};
+
+    input{
+      all:unset;
+    }
+
 
     *{
         box-sizing:border-box;
@@ -27,9 +36,10 @@ export const GlobalStyles = createGlobalStyle`
     
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size:14px;
+        background-color:#FAFAFA;
+        color:rgb(38,38,38);
     }
-
-    input{
-        all:unset;
-    }
+    
+   
 `;
