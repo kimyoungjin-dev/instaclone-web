c6f4dded4501076168cfd4b46fb47dcb553adc26
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 17:33:00 +0900

    Create path from routes.tsx

--- src/Components/AppRouter.tsx
+++ src/Components/AppRouter.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import routes from "../routes";
 import Home from "../Screens/Home";
 import Login from "../Screens/Login";
 import NotFound from "../Screens/NotFound";
+import SignUp from "../Screens/SignUp";
 
 interface IProps {
   isLoggedIn: Boolean;
@@ -12,9 +14,15 @@ const AppRouter: React.FC<IProps> = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={routes.home}>
           {isLoggedIn ? <Home /> : <Login />}
         </Route>
+
+        {!isLoggedIn ? (
+          <Route path={routes.signUp}>
+            <SignUp />
+          </Route>
+        ) : null}
 
         <Route>
           <NotFound />
