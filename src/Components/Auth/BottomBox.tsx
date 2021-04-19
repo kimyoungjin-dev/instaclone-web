cda3035e0d1ca9070b47b179f70f0a21231f8473
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-19 19:11:50 +0900

    Make a Bottom Box.
    Code recycling

---
+++ src/Components/Auth/BottomBox.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { BaseBox } from "../shared";
+
+interface IProps {
+  text: string;
+  link: string;
+  linkText: string;
+}
+
+const SBottomBox = styled(BaseBox)`
+  padding: 20px 0px;
+  text-align: center;
+  a {
+    font-weight: 600;
+    margin-left: 5px;
+    color: ${(props) => props.theme.accent};
+  }
+`;
+
+const BottomBox: React.FC<IProps> = ({ text, link, linkText }) => {
+  return (
+    <SBottomBox>
+      <span>{text}</span>
+      <Link to={link}>{linkText}</Link>
+    </SBottomBox>
+  );
+};
+
+export default BottomBox;
