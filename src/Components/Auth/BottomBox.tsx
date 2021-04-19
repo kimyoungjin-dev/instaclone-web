ffd289361f8a6a1833a4c8403f98b1bb3f637f83
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-20 00:18:33 +0900

    Reset import path

--- src/Components/Auth/BottomBox.tsx
+++ src/Components/Auth/BottomBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { BaseBox } from "../shared";
+import { BaseBox } from "../SharedStyles";
 
 interface IProps {
   text: string;
