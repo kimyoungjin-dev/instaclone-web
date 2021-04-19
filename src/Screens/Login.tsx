b3c65e2c3033bf267ac1168c47576181b2f79dd8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-20 03:02:21 +0900

    Update Login.tsx
    Overall component modification and const arrow function => change to basic function component

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -9,6 +9,8 @@ import Separator from "../Components/Auth/Separator";
 import SubmitButton from "../Components/Auth/SubmitButton";
 import routes from "../routes";
 import PageTitle from "../Components/PageTitle";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../Components/Auth/ErrorMessage";
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -18,15 +20,54 @@ const FacebookLogin = styled.div`
   }
 `;
 
-const Login = () => {
+interface IForm {
+  username: string;
+  password: string;
+}
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<IForm>({
+    mode: "onChange",
+  });
+
+  const onValid = (data: any) => console.log(data);
+
   return (
     <AuthLayout>
       <PageTitle title="Login" />
       <TopBox>
-        <form>
-          <Input type="text" placeholder="UserName" />
-          <Input type="password" placeholder="Password" />
-          <SubmitButton type="submit" value="Log in" />
+        <form onSubmit={handleSubmit(onValid)}>
+          <Input
+            {...register("username", {
+              required: true,
+              minLength: {
+                value: 5,
+                message: "Email or ID must be at least 5 characters long",
+              },
+            })}
+            type="text"
+            placeholder="UserName or Email"
+            hasError={Boolean(errors?.username?.message)}
+          />
+          <ErrorMessage message={errors?.username?.message} />
+          <Input
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 10,
+                message: "Password must be at least 10 characters long.",
+              },
+            })}
+            type="password"
+            placeholder="Password"
+            hasError={Boolean(errors?.password?.message)}
+          />
+          <ErrorMessage message={errors?.username?.message} />
+          <SubmitButton type="submit" value="Log in" disabled={!isValid} />
         </form>
         <Separator />
         <FacebookLogin>
@@ -41,5 +82,4 @@ const Login = () => {
       />
     </AuthLayout>
   );
-};
-export default Login;
+}
