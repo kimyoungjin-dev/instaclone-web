5959b6aedf19e15a99168843cfb0ace4ed016abd
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-20 22:43:19 +0900

    Temporarily clear the dark theme

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,7 +2,7 @@ import { useReactiveVar } from "@apollo/client";
 import { darkModeVar, isLoggedInVar } from "./Apollo";
 import AppRouter from "./AppRouter";
 import { ThemeProvider } from "styled-components";
-import { dark, GlobalStyles, light } from "../styles";
+import { GlobalStyles, light } from "../styles/styles";
 import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <HelmetProvider>
-      <ThemeProvider theme={darkMode ? dark : light}>
+      <ThemeProvider theme={light}>
         <GlobalStyles />
         <AppRouter isLoggedIn={isLoggedIn} />
       </ThemeProvider>
