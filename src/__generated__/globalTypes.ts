cd5955c4f22279b8770a238e11c82cfdf63cc107
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-22 20:19:37 +0900

    Create globalTypes.ts
    apllo clinet:codegen src/__generated__ —target=typescript —outputFlat

---
+++ src/__generated__/globalTypes.ts
@@ -0,0 +1,14 @@
+export {};
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
