afe67ff4d7fd5f13d38342be64bae16b2e6497d9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-23 00:13:48 +0900

    Update SignUp.tsx
    Create a SIGNUP_MUTATION and connect it with the backend.
    
    Create onSubmit and connect variables of createAccount and values of getValues.
    
    make useMutation

--- src/Screens/SignUp.tsx
+++ src/Screens/SignUp.tsx
@@ -14,8 +14,73 @@ import Form from "../Components/Auth/Box/Form";
 import Separator from "../Components/Auth/Separator";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { useForm } from "react-hook-form";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import { login, loginVariables } from "../__generated__/login";
+
+interface IForm {
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+const SIGNUP_MUTATION = gql`
+  mutation createAccount(
+    $username: String!
+    $password: String!
+    $firstName: String!
+    $lastName: String
+    $email: String!
+  ) {
+    login(
+      username: $username
+      password: $password
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+    ) {
+      ok
+      error
+    }
+  }
+`;
 
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<IForm>({
+    mode: "onChange",
+  });
+
+  const [createAccount, { loading }] = useMutation<login, loginVariables>(
+    SIGNUP_MUTATION,
+    {
+      onCompleted: (data) => {},
+    }
+  );
+
+  const onSubmit = () => {
+    const { username, password, email, firstName, lastName } = getValues();
+
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        username,
+        password,
+        email,
+        firstName,
+        lastName,
+      },
+    });
+  };
+
   return (
     <Container>
       <PageTitle title="Sign Up" />
@@ -38,12 +103,33 @@ export default function SignUp() {
           <Separator smallMargin={true} />
         </HeaderContainer>
 
-        <Form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="UserName" />
-          <Input type="password" placeholder="Password" />
-          <SubmitButton type="submit">로그인</SubmitButton>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register("firstName", { required: "First Name is required" })}
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="last Name"
+          />
+          <Input
+            {...register("email", { required: "email is required" })}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            {...register("username", { required: "username is required" })}
+            type="text"
+            placeholder="User Name"
+          />
+          <Input
+            {...register("password", { required: "password is required" })}
+            type="password"
+            placeholder="Password"
+          />
+          <SubmitButton type="submit">회원가입</SubmitButton>
         </Form>
         <AgreeText>
           가입하면 Instagram의 약관, 데이터 정책 및 쿠키 정책에 동의하게 됩니다.
