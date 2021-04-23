bb826fe3add3ee4a8439f390b92dda359e8cc113
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-23 19:01:55 +0900

    Update SignUp.tsx
    Use useHistory to make useLocation
    Receive information from Login.tsx.
    
    In the onCompleted function, it sends the message, user name, and password to the state with getValues.

--- src/Screens/SignUp.tsx
+++ src/Screens/SignUp.tsx
@@ -15,14 +15,13 @@ import Separator from "../Components/Auth/Separator";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { useForm } from "react-hook-form";
-import gql from "graphql-tag";
-import { useMutation } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 import {
   createAccount,
   createAccountVariables,
 } from "../__generated__/createAccount";
 import ErrorMessage from "../Components/Auth/ErrorMessage";
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 
 interface IForm {
   username: string;
@@ -55,43 +54,51 @@ const CREATE_ACCOUNT_MUTATION = gql`
 `;
 
 export default function SignUp() {
-  const history = useHistory();
   const {
     register,
     handleSubmit,
     setError,
     clearErrors,
-    formState: { errors },
+    getValues,
+    formState: { errors, isValid },
   } = useForm<IForm>({
     mode: "onChange",
   });
+
+  const history = useHistory();
 
   const [createAccount, { loading }] = useMutation<
     createAccount,
     createAccountVariables
   >(CREATE_ACCOUNT_MUTATION, {
     onCompleted: (data) => {
+      const { username, password } = getValues();
       const {
         createAccount: { ok, error },
       } = data;
       if (!ok) {
-        setError("createResultError", {
+        return setError("createResultError", {
           message: error || undefined,
         });
-        history.push(routes.home);
       }
+      return history.push(routes.home, {
+        message: "계정이 생성되었습니다. 로그인을 해주세요",
+        username,
+        password,
+      });
     },
   });
 
   const onSubmit = (data: IForm) => {
-    if (loading) return;
+    if (loading) {
+      return;
+    }
     createAccount({
       variables: {
         ...data,
       },
     });
   };
-  //loading중이면 return=>중단 // createAccount를 호출해서 getvaluse()와 일치시킵니다.
 
   // const clearCreateError = () => {
   //   clearErrors("createResultError");
@@ -120,7 +127,9 @@ export default function SignUp() {
 
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Input
-            {...register("firstName", { required: "First Name is required" })}
+            {...register("firstName", {
+              required: "성 은 필수 입력내용 입니다.",
+            })}
             type="text"
             placeholder="성"
             hasError={Boolean(errors?.firstName?.message)}
@@ -131,17 +140,27 @@ export default function SignUp() {
 
           <Input
             {...register("email", {
-              required: "email is required",
+              required: "이메일은 필수 입력내용 입니다.",
+              minLength: {
+                value: 5,
+                message:
+                  "이메일 형식은@를 포함하고 5글자를 이상 이어야 합니다.",
+              },
               validate: (value) => value.includes("@"),
             })}
             type="text"
             placeholder="Email"
             hasError={Boolean(errors?.email?.message)}
           />
+          {errors?.email?.type === "validate" && (
+            <ErrorMessage message="이메일 형식은 @를 포함해야 합니다." />
+          )}
           <ErrorMessage message={errors?.email?.message} />
 
           <Input
-            {...register("username", { required: "username is required" })}
+            {...register("username", {
+              required: "닉네임은 필수 입력내용 입니다.",
+            })}
             type="text"
             placeholder="닉네임"
             hasError={Boolean(errors?.username?.message)}
@@ -149,14 +168,27 @@ export default function SignUp() {
           <ErrorMessage message={errors?.username?.message} />
 
           <Input
-            {...register("password", { required: "password is required" })}
+            {...register("password", {
+              required: "비밀번호는 필수 입력내용 입니다.",
+              minLength: {
+                value: 5,
+                message: "최소 5글자이상 15글자 미만으로 입력해야 합니다.",
+              },
+              maxLength: {
+                value: 15,
+                message: "최소 5글자이상 15글자 미만으로 입력해야 합니다.",
+              },
+            })}
             type="password"
             placeholder="비밀번호"
             hasError={Boolean(errors?.password?.message)}
           />
           <ErrorMessage message={errors?.password?.message} />
 
-          <SubmitButton type="submit">회원가입</SubmitButton>
+          <SubmitButton type="submit" disabled={!isValid || loading}>
+            {loading ? "...Loading" : "회원가입"}
+          </SubmitButton>
+          <ErrorMessage message={errors?.createResultError?.message} />
         </Form>
         <AgreeText>
           가입하면 Instagram의 약관, 데이터 정책 및 쿠키 정책에 동의하게 됩니다.
