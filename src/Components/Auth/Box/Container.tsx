a6ca45bfe1e0e7de97cacb098a8d2d7a170bcbcb
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-24 16:34:24 +0900

    Added Fonts

--- src/Components/Auth/Box/Container.tsx
+++ src/Components/Auth/Box/Container.tsx
@@ -1,5 +1,9 @@
+import { useReactiveVar } from "@apollo/client";
 import styled from "styled-components";
+import { darkModeClick, darkModeVar, whiteModeClick } from "../../Apollo";
 import { ChilrenProps } from "../../interface";
+import { RiSunCloudyLine } from "react-icons/ri";
+import { GiMoonBats } from "react-icons/gi";
 
 const SContainer = styled.div`
   display: flex;
@@ -7,6 +11,7 @@ const SContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  padding-bottom: 30px;
 `;
 
 const Wrapper = styled.div`
@@ -14,9 +19,44 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const Footer = styled.div`
+  margin-bottom: 10px;
+  max-width: 350px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const DarkModeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const DarkModeBtn = styled.span`
+  font-size: 16px;
+`;
+
+const DarkModeText = styled.span`
+  font-size: 16px;
+  margin-right: 5px;
+  font-weight: 500;
+  font-family: "Orelega One", cursive;
+`;
+
 export default function Container({ children }: ChilrenProps) {
+  const darkMode = useReactiveVar(darkModeVar);
   return (
     <SContainer>
+      <Footer>
+        <DarkModeContainer onClick={darkMode ? whiteModeClick : darkModeClick}>
+          <DarkModeText>{darkMode ? "White Mode" : "Dark Mode"}</DarkModeText>
+          <DarkModeBtn>
+            {darkMode ? <RiSunCloudyLine /> : <GiMoonBats />}
+          </DarkModeBtn>
+        </DarkModeContainer>
+      </Footer>
+
       <Wrapper>{children}</Wrapper>
     </SContainer>
   );
