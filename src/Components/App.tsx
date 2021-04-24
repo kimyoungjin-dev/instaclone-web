20aa15e39e0fa2fb29ac4193c21fed13b54d3343
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-24 16:31:49 +0900

    Update App.tsx
    Delete isLoggedIn from App.tsx and load isLoggedIn from router.tsx. If it is dark mode, apply dark mode, if not dark mode, apply white mode

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,19 +2,18 @@ import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { client, darkModeVar, isLoggedInVar } from "./Apollo";
 import AppRouter from "./AppRouter";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles, light } from "../styles/styles";
+import { GlobalStyles, light, dark } from "../styles/styles";
 import { HelmetProvider } from "react-helmet-async";
 
 export default function App() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
     <ApolloProvider client={client}>
       <HelmetProvider>
-        <ThemeProvider theme={light}>
+        <ThemeProvider theme={darkMode ? dark : light}>
           <GlobalStyles />
-          <AppRouter isLoggedIn={isLoggedIn} />
+          <AppRouter />
         </ThemeProvider>
       </HelmetProvider>
     </ApolloProvider>
