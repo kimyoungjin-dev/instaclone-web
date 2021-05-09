b35925abf7ab3c488ba56903140b7298bf861ce6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-09 20:37:43 +0900

    If token exists and data.me exists, log in is maintained.
    When a component is mounted, if me is null, it logs out. Data is put in the dependency array.
    
    data: me{username, avatar} type
    skip: If the user is token===null, the query is skipped.

--- src/Components/Hooks/useUser.tsx
+++ src/Components/Hooks/useUser.tsx
@@ -1,7 +1,9 @@
 import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { useEffect } from "react";
 import { me, me_me } from "../../__generated__/me";
-import { isLoggedInVar } from "../Apollo";
+import { isLoggedInVar, logUserOut } from "../Apollo";
 
+//"사용자가 맞는지" 확인하는 me query를 백앤드로부터 불러온다.
 const ME_QUERY = gql`
   query me {
     me {
@@ -10,13 +12,20 @@ const ME_QUERY = gql`
     }
   }
 `;
-
 export default function useUser() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const { data, error } = useQuery<me, me_me>(ME_QUERY, {
-    skip: !isLoggedIn,
+  //isToken : token이 존재하는지 확인 /Boolean
+  const isToken = useReactiveVar(isLoggedInVar);
+  //data: me{username, avatar} 형태
+  //skip: user가 token===null이면 query를 건너뛴다.
+  const { data } = useQuery<me, me_me>(ME_QUERY, {
+    skip: !isToken,
   });
+  //컴포넌트가 마운트 될때, 만약 me가 null 인 경우라면 로그아웃을 해버린다.의존성 배열에는 data를 넣어준다.
+  useEffect(() => {
+    if (data?.me === null) {
+      logUserOut();
+    }
+  }, [data]);
 
-  console.log(data, error);
-  return <div></div>;
+  return <></>;
 }
