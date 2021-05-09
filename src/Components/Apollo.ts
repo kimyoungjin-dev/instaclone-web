30a693cf2eea2eca26ca91420403d8a64dfff7c9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-09 20:35:49 +0900

    Create a variable between authLink and httpLink and connect it with the concat function.
    authLink uses the setContext function,
    Add existing headers and newly added tokens.
    
    (setContext allows you to add all Requestdp items from the client.
    )

--- src/Components/Apollo.ts
+++ src/Components/Apollo.ts
@@ -1,16 +1,40 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const TOKEN = "token";
 const DARKMODE = "DARKMODE";
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+//setContext는 클라이언트의 모든 Requestdp 항목을 추가할수있게 해준다.
+//참고문서 : https://www.apollographql.com/docs/react/networking/advanced-http-networking/#gatsby-focus-wrapper
+//참고문서 : https://www.apollographql.com/docs/react/api/link/apollo-link-context/#overview
+//setContext의 첫번째인자는 GraphQL요청(현재는 사용x)
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: localStorage.getItem(TOKEN),
+    },
+  };
 });
 
-// //login
-export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
 
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  //authLink와 httlLink를 하나로 묶는다.
+  link: authLink.concat(httpLink),
+});
+
+//isLoggedInVar
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+//login
 export const logUserIn = (token: string) => {
   localStorage.setItem(TOKEN, token);
   isLoggedInVar(true);
@@ -24,14 +48,11 @@ export const logUserOut = () => {
 };
 
 //darkmode
-
 export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARKMODE)));
-
 export const darkModeClick = () => {
   localStorage.setItem(DARKMODE, "DARKMODE");
   darkModeVar(true);
 };
-
 export const whiteModeClick = () => {
   localStorage.removeItem(DARKMODE);
 
