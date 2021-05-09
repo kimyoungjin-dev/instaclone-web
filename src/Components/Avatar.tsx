64006646d3191dca338dfb57e5e8d17281785f6c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-09 22:29:53 +0900

    Create an Avatar.tsx component that renders an avatar that a user has.
    Basically, the avatar may not exist, so an empty array is passed and if the avatar address exists, the corresponding address is rendered.

---
+++ src/Components/Avatar.tsx
@@ -0,0 +1,18 @@
+import { BiUserCircle } from "react-icons/bi";
+import styled from "styled-components";
+
+const SAvatar = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: #2c2c2c;
+  overflow: hidden;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+`;
+//기본적으로 user아이콘을 보여주고, 만약 url이 존재한다면 header컴포넌트로 부터 url 을 받는다.
+export default function Avatar({ url = "" }) {
+  return <SAvatar>{url === "" ? <BiUserCircle /> : <Img src={url} />}</SAvatar>;
+}
