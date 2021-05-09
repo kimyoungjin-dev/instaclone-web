07397072ec4b2416b17f90095434f165c7266e62
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-10 03:13:57 +0900

    Create lg props and specify the type.
    Specifies the type of lg and url as an interface.
    
    Using lg props, adjust the width and height of the user's avatar photo as a boolean value.

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -1,18 +1,31 @@
 import { BiUserCircle } from "react-icons/bi";
 import styled from "styled-components";
 
-const SAvatar = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
+interface IProps {
+  lg: boolean;
+  url: string | undefined;
+}
+
+const SAvatar = styled.div<{ lg: boolean }>`
+  width: ${(props) => (props.lg ? "35px" : "26px")};
+  height: ${(props) => (props.lg ? "35x" : "23px")};
+  border-radius: 100%;
   overflow: hidden;
-  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Img = styled.img`
-  max-width: 100%;
+  width: 100%;
+  height: 35px;
+  border-radius: 100%;
 `;
 //기본적으로 user아이콘을 보여주고, 만약 url이 존재한다면 header컴포넌트로 부터 url 을 받는다.
-export default function Avatar({ url = "" }) {
-  return <SAvatar>{url === "" ? <BiUserCircle /> : <Img src={url} />}</SAvatar>;
+export default function Avatar({ url = "", lg = false }: IProps) {
+  return (
+    <SAvatar lg={lg}>
+      {url === "" ? <BiUserCircle /> : <Img src={url} />}
+    </SAvatar>
+  );
 }
