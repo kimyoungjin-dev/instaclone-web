f6a9220c3689b1725975a3bc7a9de9f5e4aa1ec3
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-09 18:09:26 +0900

    Change the file location in the header folder. Render the icon using the map function.

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../Apollo";
+import { Link } from "react-router-dom";
+import routes from "../../routes";
+import { iconArray } from "./IconData";
+
+const Container = styled.div`
+  border-bottom: 0.5px solid ${(props) => props.theme.silverColor};
+  height: 52px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  max-width: 930px;
+  width: 100%;
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Column = styled.div`
+  h1 {
+    font-size: 25px;
+    font-family: "Caveat Brush", cursive;
+  }
+`;
+
+const Button = styled.span`
+  padding: 5px;
+  background-color: ${(props) => props.theme.loginBtnColor};
+  color: white;
+  font-weight: 600;
+  font-size: 14px;
+  border-radius: 3px;
+`;
+
+const Icon = styled.span`
+  margin-left: 25px;
+  font-size: 20px;
+`;
+
+export default function Header() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Column>
+          <h1>Instaram</h1>
+        </Column>
+        <Column>
+          {isLoggedIn ? (
+            <>
+              {iconArray.map((icon) => (
+                <Icon>
+                  <icon.name />
+                </Icon>
+              ))}
+            </>
+          ) : (
+            <Link to={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
+        </Column>
+      </Wrapper>
+    </Container>
+  );
+}
