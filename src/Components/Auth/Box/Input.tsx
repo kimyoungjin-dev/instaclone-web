0af3f684811fa440c5e400b86f0199e39112e823
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-12 21:00:47 +0900

    Modify CSS

--- src/Components/Auth/Box/Input.tsx
+++ src/Components/Auth/Box/Input.tsx
@@ -12,6 +12,7 @@ const Input = styled.input<IProps>`
   margin-top: 5px;
   box-sizing: border-box;
   transition: 0.6s ease-in-out;
+  color: black;
   border: 0.5px solid
     ${(props) => (props.hasError ? "red" : props.theme.borderColor)};
   &::placeholder {
