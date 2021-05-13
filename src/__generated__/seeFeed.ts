7dcc3ae7259aca7773bde82971b7b85cdaa4e750
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-13 19:29:45 +0900

    npm run codegen
    In order to write a comment to a query in seeFeed, an id must exist,
    
    Retrieve user information from seeFeed=>comments.

--- src/__generated__/seeFeed.ts
+++ src/__generated__/seeFeed.ts
@@ -13,6 +13,21 @@ export interface seeFeed_seeFeed_user {
   avatar: string | null;
 }
 
+export interface seeFeed_seeFeed_comments_user {
+  __typename: "User";
+  username: string;
+  avatar: string | null;
+}
+
+export interface seeFeed_seeFeed_comments {
+  __typename: "Comment";
+  id: number;
+  user: seeFeed_seeFeed_comments_user;
+  payload: string;
+  isMine: boolean;
+  createdAt: string;
+}
+
 export interface seeFeed_seeFeed {
   __typename: "Photo";
   id: number;
@@ -20,7 +35,8 @@ export interface seeFeed_seeFeed {
   file: string;
   caption: string | null;
   likes: number;
-  comments: number;
+  commentNumber: number;
+  comments: (seeFeed_seeFeed_comments | null)[] | null;
   createdAt: string;
   isMine: boolean;
   isLiked: boolean;
