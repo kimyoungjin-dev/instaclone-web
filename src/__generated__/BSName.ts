68e67c8a547810142f3bbe8cf88bf7513b234b22
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-13 19:30:37 +0900

    Codegen execution => Create BSName type to access from cache.

---
+++ src/__generated__/BSName.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: BSName
+// ====================================================
+
+export interface BSName {
+  __typename: "Photo";
+  likes: number;
+  isLiked: boolean;
+}
