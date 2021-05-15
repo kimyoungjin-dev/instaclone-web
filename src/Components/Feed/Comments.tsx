1b34017f2aa808e329c235191b6e8fd70ae80f27
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-15 22:11:32 +0900

    Create fake comments, write comments in real time, and increase commentsCount by +1.
    Note that if setValue is placed before getValues, payload will be an empty string.
    
    Use useUser to match the user information of newComment and the information of useUser.

--- src/Components/Feed/Comments.tsx
+++ src/Components/Feed/Comments.tsx
@@ -7,12 +7,14 @@ import {
   seeFeed_seeFeed,
   seeFeed_seeFeed_user,
 } from "../../__generated__/seeFeed";
+import useUser from "../Hooks/useUser";
 import Comment from "./Comment";
 
 const CREAT_COMMENT_MUTATION = gql`
   mutation createComment($photoId: Int!, $payload: String!) {
     createComment(photoId: $photoId, payload: $payload) {
       ok
+      id
       error
     }
   }
@@ -43,11 +45,51 @@ export default function Comments({
   caption,
   photoId,
 }: UpdatedProps) {
+  const { register, handleSubmit, setValue, getValues } = useForm();
+  const { data: userData } = useUser();
   const [createCommentMutation, { loading }] = useMutation(
-    CREAT_COMMENT_MUTATION
-  );
+    CREAT_COMMENT_MUTATION,
+    {
+      update: (cache, result) => {
+        //setValue가 getValues보다 앞에 위치한다면 payload:"" 가됨
+        const { payload } = getValues();
+        setValue("payload", "");
+        if (result.data.createComment.ok) {
+          const {
+            data: {
+              createComment: { ok, id },
+            },
+          } = result;
+          if (ok && userData?.me) {
+            const newComment = {
+              //useUser의 __typenamed을 추가해주어야한다.
+              __typename: "Comment",
+              createdAt: Date.now() + "",
+              id,
+              isMine: true,
+              payload,
+              user: { ...userData?.me },
+            };
+            cache.modify({
+              //수정할 아이디 : photoId
+              //혹여나 :를 붙이는것이 특히 주의해야함
+              id: `Photo:${photoId}`,
+              //fields object안에 모든 프로퍼티는 fuc형태
+              fields: {
+                comments(prev) {
+                  return [...prev, newComment];
+                },
 
-  const { register, handleSubmit, setValue } = useForm();
+                commentNumber(prev) {
+                  return prev + 1;
+                },
+              },
+            });
+          }
+        }
+      },
+    }
+  );
 
   //loading? return //!loading :
   const onSubmit: SubmitHandler<createCommentVariables> = (data) => {
@@ -61,8 +103,6 @@ export default function Comments({
         payload,
       },
     });
-    //setValue를 이용해서 submit 이후에 input 을 지운다.
-    setValue("payload", "");
   };
 
   return (
