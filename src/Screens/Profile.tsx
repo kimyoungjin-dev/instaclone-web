3ba9b430dc3368604afc88d510fcc557e21d0a3d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-16 02:34:47 +0900

    make a seeProfile query

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import { useParams } from "react-router";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      firstName
+      lastName
+      username
+      bio
+      avatar
+      photos {
+        id
+        file
+        likes
+        commentNumber
+        isLiked
+      }
+      totalFollowing
+      totalFollowers
+      isMe
+      isFollowing
+    }
+  }
+`;
 
 export default function Profile() {
   const params = useParams();
-  console.log(params);
+  const { data } = useQuery(SEE_PROFILE_QUERY);
+
   return <div>Profile</div>;
 }
