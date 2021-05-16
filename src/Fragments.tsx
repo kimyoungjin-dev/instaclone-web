faf364352127005bcc60144d72f6520138c3c951
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-16 17:09:52 +0900

    Recycle frequently used values in photo or comment

--- src/Fragments.tsx
+++ src/Fragments.tsx
@@ -1,4 +1,24 @@
-import React from "react";
+import { gql } from "@apollo/client";
 
-export default function Fragments() {
-}
+export const PHOTO_FRAGMENT = gql`
+  fragment PhotoFragment on Photo {
+    id
+    file
+    likes
+    commentNumber
+    isLiked
+  }
+`;
+
+export const COMMENT_FRAGMENT = gql`
+  fragment CommentFragment on Comment {
+    id
+    user {
+      username
+      avatar
+    }
+    payload
+    isMine
+    createdAt
+  }
+`;
