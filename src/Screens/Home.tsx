6c000750ea3859230a695ad7dcfb9a547fa23f32
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-16 23:32:19 +0900

    Code cleanup
    Create the StyledComponents folder and move the styledcomponents part of the component.

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,7 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import Photo from "../Components/Feed/Photo";
 import PageTitle from "../Components/PageTitle";
-import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../Fragments";
+import { FEED_QUERY } from "../Fragments";
 import { seeFeed, seeFeedVariables } from "../__generated__/seeFeed";
 import styled from "styled-components";
 import DarkModeContainer from "../Components/DarkModeContainer";
@@ -10,31 +10,6 @@ const Cotainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const FEED_QUERY = gql`
-  query seeFeed($page: Int!) {
-    seeFeed(page: $page) {
-      ...PhotoFragment
-      id
-      user {
-        username
-        avatar
-      }
-      file
-      caption
-      likes
-      commentNumber
-      comments {
-        ...CommentFragment
-      }
-      createdAt
-      isMine
-      isLiked
-    }
-  }
-  ${COMMENT_FRAGMENT}
-  ${PHOTO_FRAGMENT}
 `;
 
 //seeFeed 외에 변수가 있는경우에만 <seeFeed , "">추가로 써준다.
--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,163 +1,33 @@
 import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
 import { useParams } from "react-router";
-import { PHOTO_FRAGMENT } from "../Fragments";
+import { SEE_PROFILE_QUERY } from "../Fragments";
 import {
   seeProfile,
   seeProfileVariables,
   seeProfile_seeProfile,
 } from "../__generated__/seeProfile";
-import styled from "styled-components";
 import { AiFillHeart, AiOutlineHeart, AiTwotoneMessage } from "react-icons/ai";
 import { BiMessageRounded } from "react-icons/bi";
 import DarkModeContainer from "../Components/DarkModeContainer";
 import PageTitle from "../Components/PageTitle";
-import Button from "../Components/Auth/Box/SubmitButton";
-
-const FOLLOW_USER_MUTATION = gql`
-  mutation followUser($username: String!) {
-    followUser(username: $username) {
-      ok
-    }
-  }
-`;
-
-const UNFOLLOW_USER_MUTATION = gql`
-  mutation unfollowUser($username: String!) {
-    unfollowUser(username: $username) {
-      ok
-    }
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const Wrapper = styled.div`
-  width: 700px;
-`;
-
-const ProfileContainer = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  margin-bottom: 10px;
-`;
-
-const UserInfoMation = styled.div`
-  display: flex;
-`;
-
-const ProfilePhoto = styled.img`
-  width: 150px;
-  height: 150px;
-  border-radius: 100%;
-  border: 1px solid ${(props) => props.theme.borderColor};
-`;
-
-const InfoMationBox = styled.div`
-  margin-left: 30px;
-  line-height: 2;
-`;
-
-const UserName = styled.span`
-  font-weight: bold;
-  font-size: 20px;
-`;
-
-const FollowBox = styled.div`
-  display: flex;
-`;
-
-const Follow = styled.span`
-  color: gray;
-  opacity: 0.8;
-  :not(:first-child) {
-    margin-left: 10px;
-  }
-`;
-
-const Maintitle = styled.div`
-  font-size: 18px;
-`;
-
-const Subtitle = styled.div`
-  font-size: 15px;
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  margin-top: 50px;
-  row-gap: 30px;
-  column-gap: 20px;
-`;
-
-const Photo = styled.div<{ bg: string | undefined }>`
-  position: relative;
-  background-size: cover;
-  background-position: center center;
-  background-image: url(${(props) => props.bg});
-  width: 220px;
-  height: 220px;
-  border-radius: 3px;
-`;
-
-const Icons = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
-  transition: 0.4s ease-in-out;
-  opacity: 0;
-  :hover {
-    opacity: 1;
-  }
-`;
-
-const Icon = styled.span`
-  margin-left: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  svg {
-    margin-right: 3px;
-  }
-`;
-
-const EditBtn = styled(Button)`
-  width: 70px;
-  height: 20px;
-  margin-left: 5px;
-  padding: 3px;
-  font-size: 12px;
-`;
-
-const SEE_PROFILE_QUERY = gql`
-  query seeProfile($username: String!) {
-    seeProfile(username: $username) {
-      firstName
-      lastName
-      username
-      bio
-      avatar
-      photos {
-        ...PhotoFragment
-      }
-
-      totalFollowing
-      totalFollowers
-      isMe
-      isFollowing
-    }
-  }
-  ${PHOTO_FRAGMENT}
-`;
+import {
+  Container,
+  EditBtn,
+  Follow,
+  FollowBox,
+  Grid,
+  Icon,
+  Icons,
+  InfoMationBox,
+  Maintitle,
+  ProfileContainer,
+  ProfilePhoto,
+  UserInfoMation,
+  UserName,
+  Wrapper,
+  Photo,
+  Subtitle,
+} from "../styles/StyledComponents/ProfileStyle";
 
 export default function Profile() {
   const { username } = useParams<seeProfileVariables>(); //{username:youngjin}
@@ -170,9 +40,7 @@ export default function Profile() {
     }
   );
 
-  //{data?.seeProfile ? getButton(data.seeProfile) : null}
-  //seeProfile이 존재를 한다면 getButton호출, 존재하지않는다면 null
-  const getButton = (seeProfile: seeProfile_seeProfile) => {
+  const checkFn = (seeProfile: seeProfile_seeProfile) => {
     const { isMe, isFollowing } = seeProfile;
     if (isMe) return <EditBtn>Edit Profile</EditBtn>;
     if (isFollowing) return <EditBtn>UnFollwing</EditBtn>;
@@ -197,7 +65,7 @@ export default function Profile() {
             <ProfilePhoto src={data?.seeProfile?.avatar || undefined} />
             <InfoMationBox>
               <UserName>{data?.seeProfile?.username}</UserName>
-              {data?.seeProfile ? getButton(data.seeProfile) : null}
+              {data?.seeProfile ? checkFn(data.seeProfile) : null}
 
               <FollowBox>
                 <Follow>{data?.seeProfile?.totalFollowers} Followers</Follow>
