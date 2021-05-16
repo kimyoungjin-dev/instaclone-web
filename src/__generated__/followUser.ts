fbdf723fc8ad49cf7553371fc15e25a04ef42df0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-17 01:16:29 +0900

    npm run codgen run => Specifies the type of unfollowUser and followUser.

---
+++ src/__generated__/followUser.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: followUser
+// ====================================================
+
+export interface followUser_followUser {
+  __typename: "MutationResponse";
+  ok: boolean;
+}
+
+export interface followUser {
+  followUser: followUser_followUser;
+}
+
+export interface followUserVariables {
+  username: string;
+}
---
+++ src/__generated__/unfollowUser.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: unfollowUser
+// ====================================================
+
+export interface unfollowUser_unfollowUser {
+  __typename: "MutationResponse";
+  ok: boolean;
+}
+
+export interface unfollowUser {
+  unfollowUser: unfollowUser_unfollowUser;
+}
+
+export interface unfollowUserVariables {
+  username: string;
+}
