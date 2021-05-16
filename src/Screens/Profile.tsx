a4606cd71c6d11d2fb12e3c9dc25e078e13a2434
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-16 22:42:41 +0900

    Update Profile.tsx
    Create FOLLOW_USER_MUTATION,UNFOLLOW_USER_MUTATION.
    
    If there are multiple conditional statements, make the getButton function to increase the readability of the code.
    
    In the case of isMe, set the Edit Profile button to be visible.
    
    If isFollowing is set, "Unfollowing" and vice versa, make the text "following" visible.
    
    Add PageTitle: "Loading" if loading, make username visible if not loading.

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -2,12 +2,33 @@ import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import { useParams } from "react-router";
 import { PHOTO_FRAGMENT } from "../Fragments";
-import { seeProfile, seeProfileVariables } from "../__generated__/seeProfile";
+import {
+  seeProfile,
+  seeProfileVariables,
+  seeProfile_seeProfile,
+} from "../__generated__/seeProfile";
 import styled from "styled-components";
 import { AiFillHeart, AiOutlineHeart, AiTwotoneMessage } from "react-icons/ai";
 import { BiMessageRounded } from "react-icons/bi";
 import DarkModeContainer from "../Components/DarkModeContainer";
 import PageTitle from "../Components/PageTitle";
+import Button from "../Components/Auth/Box/SubmitButton";
+
+const FOLLOW_USER_MUTATION = gql`
+  mutation followUser($username: String!) {
+    followUser(username: $username) {
+      ok
+    }
+  }
+`;
+
+const UNFOLLOW_USER_MUTATION = gql`
+  mutation unfollowUser($username: String!) {
+    unfollowUser(username: $username) {
+      ok
+    }
+  }
+`;
 
 const Container = styled.div`
   display: flex;
@@ -23,11 +44,6 @@ const Wrapper = styled.div`
 const ProfileContainer = styled.div`
   background-color: ${(props) => props.theme.bgColor};
   margin-bottom: 10px;
-`;
-
-const ProfileDarkModeBox = styled.div`
-  display: flex;
-  justify-content: flex-end;
 `;
 
 const UserInfoMation = styled.div`
@@ -114,6 +130,14 @@ const Icon = styled.span`
   }
 `;
 
+const EditBtn = styled(Button)`
+  width: 70px;
+  height: 20px;
+  margin-left: 5px;
+  padding: 3px;
+  font-size: 12px;
+`;
+
 const SEE_PROFILE_QUERY = gql`
   query seeProfile($username: String!) {
     seeProfile(username: $username) {
@@ -137,7 +161,7 @@ const SEE_PROFILE_QUERY = gql`
 
 export default function Profile() {
   const { username } = useParams<seeProfileVariables>(); //{username:youngjin}
-  const { data } = useQuery<seeProfile, seeProfileVariables>(
+  const { data, loading } = useQuery<seeProfile, seeProfileVariables>(
     SEE_PROFILE_QUERY,
     {
       variables: {
@@ -146,23 +170,40 @@ export default function Profile() {
     }
   );
 
+  //{data?.seeProfile ? getButton(data.seeProfile) : null}
+  //seeProfile이 존재를 한다면 getButton호출, 존재하지않는다면 null
+  const getButton = (seeProfile: seeProfile_seeProfile) => {
+    const { isMe, isFollowing } = seeProfile;
+    if (isMe) return <EditBtn>Edit Profile</EditBtn>;
+    if (isFollowing) return <EditBtn>UnFollwing</EditBtn>;
+    else {
+      return <EditBtn>Follow</EditBtn>;
+    }
+  };
+
   return (
     <Container>
-      <PageTitle title="Profile" />
+      <PageTitle
+        title={
+          loading ? "Loading..." : `${data?.seeProfile?.username}'s Profile`
+        }
+      />
+
       <Wrapper>
-        <ProfileDarkModeBox>
-          <DarkModeContainer />
-        </ProfileDarkModeBox>
+        <DarkModeContainer />
 
         <ProfileContainer>
           <UserInfoMation>
             <ProfilePhoto src={data?.seeProfile?.avatar || undefined} />
             <InfoMationBox>
               <UserName>{data?.seeProfile?.username}</UserName>
+              {data?.seeProfile ? getButton(data.seeProfile) : null}
+
               <FollowBox>
                 <Follow>{data?.seeProfile?.totalFollowers} Followers</Follow>
                 <Follow>{data?.seeProfile?.totalFollowing} Following</Follow>
               </FollowBox>
+
               <Maintitle>{data?.seeProfile?.username} Should Work!</Maintitle>
               <Subtitle>Super Exciting!</Subtitle>
             </InfoMationBox>
@@ -170,7 +211,7 @@ export default function Profile() {
 
           <Grid>
             {data?.seeProfile?.photos?.map((photo) => (
-              <Photo bg={photo?.file || undefined}>
+              <Photo key={photo?.id} bg={photo?.file || undefined}>
                 <Icons>
                   <Icon>
                     {photo?.likes === 0 ? <AiOutlineHeart /> : <AiFillHeart />}
