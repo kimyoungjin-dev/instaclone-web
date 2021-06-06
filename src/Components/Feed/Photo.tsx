fb99cc330b238a9e5914fe3915b3a379d1c95004
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-07 06:48:15 +0900

    Separate the part responsible for Photo in Home.tsx and make it a component.

---
+++ src/Components/Feed/Photo.tsx
@@ -0,0 +1,108 @@
+import { AiFillHeart, AiOutlineHeart, AiTwotoneMessage } from "react-icons/ai";
+import { BsBookmark, BsThreeDots } from "react-icons/bs";
+import { HiOutlinePaperAirplane } from "react-icons/hi";
+import styled from "styled-components";
+import { seeFeed_seeFeed } from "../../__generated__/seeFeed";
+import Avatar from "../Avatar";
+import Likes from "../Likes/Likes";
+import { FatText } from "../SharedStyles";
+
+const PhotoCotainer = styled.div`
+  border: 1px solid ${(props) => props.theme.borderColor};
+  width: 700px;
+  border-radius: 8px;
+  margin-bottom: 25px;
+`;
+
+const PhotoHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+
+  & svg {
+    font-size: 24px;
+    cursor: pointer;
+  }
+`;
+
+const UserName = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 15px;
+`;
+
+const PhotoAction = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+`;
+
+const ActionColumn = styled.div`
+  & svg {
+    font-size: 21px;
+  }
+  & :not(:first-child) {
+    margin-left: 15px;
+  }
+`;
+
+type PhotoPick = Pick<
+  seeFeed_seeFeed,
+  "id" | "user" | "likes" | "file" | "isLiked"
+>;
+
+export default function Photo({ id, user, likes, file, isLiked }: PhotoPick) {
+  return (
+    <PhotoCotainer key={id}>
+      <PhotoHeader>
+        <Column>
+          <Avatar url={user.avatar || undefined} />
+          <UserName>{user.username}</UserName>
+        </Column>
+
+        <Column>
+          <BsThreeDots />
+        </Column>
+      </PhotoHeader>
+
+      <PhotoFile src={file || undefined} />
+
+      <PhotoData>
+        <PhotoAction>
+          <ActionColumn>
+            {isLiked ? (
+              <AiFillHeart style={{ color: "tomato" }} />
+            ) : (
+              <AiOutlineHeart />
+            )}
+            <AiTwotoneMessage />
+            <HiOutlinePaperAirplane />
+          </ActionColumn>
+
+          <ActionColumn>
+            <BsThreeDots />
+          </ActionColumn>
+
+          <ActionColumn>
+            <BsBookmark />
+          </ActionColumn>
+        </PhotoAction>
+
+        <Likes photoId={id} likes={likes} />
+      </PhotoData>
+    </PhotoCotainer>
+  );
+}
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import { useQuery } from "@apollo/client";
-import { FEED_QUERY, SEE_PHOTO_LIKES } from "../Components/Fragment";
+import { FEED_QUERY } from "../Components/Fragment";
 import Avatar from "../Components/Avatar";
 import { seeFeed } from "../__generated__/seeFeed";
 import { FatText } from "../Components/SharedStyles";
-import { AiOutlineHeart, AiTwotoneMessage } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart, AiTwotoneMessage } from "react-icons/ai";
 import { HiOutlinePaperAirplane } from "react-icons/hi";
 import { BsBookmark, BsThreeDots } from "react-icons/bs";
 import Likes from "../Components/Likes/Likes";
+import Photo from "../Components/Feed/Photo";
 
 const Box = styled.div`
   display: flex;
@@ -16,104 +17,17 @@ const Box = styled.div`
   flex-direction: column;
 `;
 
-const PhotoCotainer = styled.div`
-  border: 1px solid ${(props) => props.theme.borderColor};
-  width: 700px;
-  border-radius: 8px;
-  margin-bottom: 25px;
-`;
-
-const PhotoHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-`;
-
-const Column = styled.div`
-  display: flex;
-  align-items: center;
-
-  & svg {
-    font-size: 24px;
-    cursor: pointer;
-  }
-`;
-
-const UserName = styled(FatText)`
-  margin-left: 15px;
-`;
-
-const PhotoFile = styled.img`
-  width: 100%;
-`;
-
-const PhotoData = styled.div`
-  padding: 15px;
-`;
-
-const PhotoAction = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 8px;
-`;
-
-const ActionColumn = styled.div`
-  & svg {
-    font-size: 21px;
-  }
-  & :not(:first-child) {
-    margin-left: 15px;
-  }
-`;
-
-const PhotoLikes = styled.span``;
-
 export default function Home() {
-  const { data, loading } = useQuery<seeFeed>(FEED_QUERY, {
+  const { data } = useQuery<seeFeed>(FEED_QUERY, {
     variables: {
-      offset: 1,
+      offset: 0,
     },
   });
-
+  console.log(data);
   return (
     <Box>
       {data?.seeFeed?.map((photo) => (
-        <PhotoCotainer>
-          <PhotoHeader>
-            <Column>
-              <Avatar url={photo?.user.avatar || undefined} />
-              <UserName>{photo?.user.username}</UserName>
-            </Column>
-
-            <Column>
-              <BsThreeDots />
-            </Column>
-          </PhotoHeader>
-
-          <PhotoFile src={photo?.file || undefined} />
-
-          <PhotoData>
-            <PhotoAction>
-              <ActionColumn>
-                <AiOutlineHeart />
-                <AiTwotoneMessage />
-                <HiOutlinePaperAirplane />
-              </ActionColumn>
-
-              <ActionColumn>
-                <BsThreeDots />
-              </ActionColumn>
-
-              <ActionColumn>
-                <BsBookmark />
-              </ActionColumn>
-            </PhotoAction>
-
-            <Likes photoId={photo?.id!} likes={photo?.likes!} />
-          </PhotoData>
-        </PhotoCotainer>
+        <Photo key={photo?.id} {...photo!} />
       ))}
     </Box>
   );
