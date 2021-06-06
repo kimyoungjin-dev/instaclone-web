15df1934bc15f6a5835ed990ce0eeb1eed9dc1dc
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-07 04:26:21 +0900

    Start review

--- src/Components/Auth/Login/LoginInterface.tsx
+++
@@ -1,6 +0,0 @@
-import { loginVariables } from "../../../__generated__/login";
-
-export interface LoginInterFace extends loginVariables {
-  resultError: string;
-  message: string;
-}
--- src/Components/Auth/SignUp/SignUpInterface.tsx
+++
@@ -1,5 +0,0 @@
-import { createAccountVariables } from "../../../__generated__/createAccount";
-
-export interface SignUpProps extends createAccountVariables {
-  createResultError: string;
-}
