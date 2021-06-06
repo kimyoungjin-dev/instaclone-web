3e1cc50b39cf41a3cb793f4db7a36e3333fe97ca
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-07 08:25:03 +0900

    Create NumberColor and color the number

--- src/Components/Likes/Likes.tsx
+++ src/Components/Likes/Likes.tsx
@@ -7,6 +7,7 @@ import {
 import Avatar from "../Avatar";
 import { SEE_PHOTO_LIKES } from "../Fragment";
 import styled from "styled-components";
+import { NumberColor } from "../SharedStyles";
 
 interface PhotoPick {
   photoId: seeFeed_seeFeed["id"];
@@ -22,10 +23,6 @@ const LikeUser = styled.span`
   display: block;
   margin: 0px 7px;
   font-size: 12px;
-`;
-
-const LikeColor = styled.span`
-  color: tomato;
 `;
 
 export default function Likes({ photoId, likes }: PhotoPick) {
@@ -46,14 +43,14 @@ export default function Likes({ photoId, likes }: PhotoPick) {
             <LikeBox>
               <Avatar url={like?.avatar || undefined} />
               <LikeUser>{like?.username}</LikeUser>
-              <LikeColor style={{ marginRight: 5 }}>1</LikeColor> Like
+              <NumberColor style={{ marginRight: 5 }}>1</NumberColor> Like
             </LikeBox>
           ) : (
             index === 0 && (
               <LikeBox style={{ display: "flex", alignItems: "center" }}>
                 <Avatar url={like?.avatar || undefined} />
                 <div style={{ marginLeft: 10 }}>
-                  {like?.username}님 외에 <LikeColor>{likes - 1}</LikeColor>
+                  {like?.username}님 외에 <NumberColor>{likes - 1}</NumberColor>
                   명이 좋아요를 눌렀습니다.
                 </div>
               </LikeBox>
