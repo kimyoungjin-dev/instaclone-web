a9e603efc6fdfd01c926d8d03e2667fae7cd6ae1
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-07 08:57:01 +0900

    Code Cleanup

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,13 +1,7 @@
 import styled from "styled-components";
 import { useQuery } from "@apollo/client";
 import { FEED_QUERY } from "../Components/Fragment";
-import Avatar from "../Components/Avatar";
 import { seeFeed } from "../__generated__/seeFeed";
-import { FatText } from "../Components/SharedStyles";
-import { AiOutlineHeart, AiFillHeart, AiTwotoneMessage } from "react-icons/ai";
-import { HiOutlinePaperAirplane } from "react-icons/hi";
-import { BsBookmark, BsThreeDots } from "react-icons/bs";
-import Likes from "../Components/Likes/Likes";
 import Photo from "../Components/Feed/Photo";
 
 const Box = styled.div`
