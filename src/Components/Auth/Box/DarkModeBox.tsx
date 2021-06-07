db28b1315162578f39b81395ae5b7e20f3791547
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-07 12:34:15 +0900

    error correction

--- src/Components/Auth/Box/DarkModeBox.tsx
+++ src/Components/Auth/Box/DarkModeBox.tsx
@@ -4,9 +4,7 @@ import { darkModeClick, darkModeVar, whiteModeClick } from "../../Apollo";
 import { BsMoon } from "react-icons/bs";
 import { BiMoon } from "react-icons/bi";
 
-const Text = styled.span`
-  cursor: pointer;
-  width: 100%;
+const Container = styled.div`
   text-align: right;
   font-size: 16px;
   font-weight: bold;
@@ -20,22 +18,23 @@ const Box = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 10px;
+  cursor: pointer;
 `;
 
 export default function DarkModeBox() {
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <Text onClick={() => (darkMode ? whiteModeClick() : darkModeClick())}>
+    <Container onClick={() => (darkMode ? whiteModeClick() : darkModeClick())}>
       {darkMode ? (
         <Box>
-          <BiMoon /> <span>Dark Mode</span>
+          <BsMoon /> <span>White Mode</span>
         </Box>
       ) : (
         <Box>
-          <BiMoon /> <span>Light Mode</span>
+          <BiMoon /> <span>Dark Mode</span>
         </Box>
       )}
-    </Text>
+    </Container>
   );
 }
