79975e602fd62d9d5d9a00cef9cb0c87eb996532
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-10 08:39:06 +0900

    Implement pagination. Create previous and next buttons.

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { FEED_QUERY } from "../Components/Fragment";
 import { seeFeed } from "../__generated__/seeFeed";
 import Photo from "../Components/Feed/Photo";
+import React, { useState } from "react";
 
 const Box = styled.div`
   display: flex;
@@ -11,18 +12,75 @@ const Box = styled.div`
   flex-direction: column;
 `;
 
+const PageNation = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const PageError = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 30px;
+  color: tomato;
+`;
+
+const PageText = styled.span`
+  cursor: pointer;
+  font-weight: bold;
+`;
+
 export default function Home() {
+  const [offset, setOffset] = useState(1);
+
   const { data } = useQuery<seeFeed>(FEED_QUERY, {
     variables: {
-      offset: 0,
+      offset,
     },
   });
 
+  const nextPage = () => {
+    setOffset((offset) =>
+      data?.seeFeed?.length !== 0 ? offset + 1 : offset - 1
+    );
+  };
+
+  const prevPage = () => {
+    setOffset((offset) => offset - 1);
+  };
+
+  const firstPage = () => {
+    setOffset((offset) => (offset = 1));
+  };
+
   return (
-    <Box>
-      {data?.seeFeed?.map((photo) => (
-        <Photo key={photo?.id} {...photo!} />
-      ))}
-    </Box>
+    <>
+      <Box>
+        {data?.seeFeed?.map((photo) => (
+          <Photo key={photo?.id} {...photo!} />
+        ))}
+      </Box>
+      <PageNation>
+        {data?.seeFeed?.length === 0 ? (
+          <React.Fragment>
+            <PageError>더이상 페이지가 존재하지 않습니다.</PageError>
+            <PageText onClick={firstPage} style={{ marginBottom: 30 }}>
+              처음페이지로 돌아가기
+            </PageText>
+          </React.Fragment>
+        ) : null}
+
+        <div style={{ paddingBottom: 30 }}>
+          <PageText onClick={prevPage} style={{ marginRight: 20 }}>
+            이전 페이지로
+          </PageText>
+
+          <PageText onClick={nextPage}>
+            {data?.seeFeed?.length === 0 ? null : "다음 페이지로"}
+          </PageText>
+        </div>
+      </PageNation>
+    </>
   );
 }
