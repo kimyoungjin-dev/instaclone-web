ed14dd0beef0dad41bf64aadb02cd23f13699c5c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-10 08:37:44 +0900

    Move the location of Profile.tsx & Hashtag.tsx into the screen folder.

--- src/Components/Feed/Hashtag.tsx
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@apollo/client";
-import { SEE_HASH_TAGS } from "../Fragment";
-
-export default function Hashtag() {
-  const { data } = useQuery(SEE_HASH_TAGS);
-  return <div>Hashtag</div>;
-}
