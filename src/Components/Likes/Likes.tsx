e4274a85edfd2ed815a9be89d6f5c337b5befba8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-06-11 19:04:59 +0900

    Click the like text => You can see the list of users who have liked.

--- src/Components/Likes/Likes.tsx
+++ src/Components/Likes/Likes.tsx
@@ -4,13 +4,13 @@ import {
   seePhotoLikes,
   seePhotoLikesVariables,
 } from "../../__generated__/seePhotoLikes";
-import Avatar from "../Avatar";
 import { SEE_PHOTO_LIKES } from "../Fragment";
 import styled from "styled-components";
-import { NumberColor } from "../SharedStyles";
-import { Link } from "react-router-dom";
+import { defaultFlexBox, NumberColor } from "../SharedStyles";
 import routes from "../../routes";
 import LikesLink from "./LikesLink";
+import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 interface PhotoPick {
   photoId: seeFeed_seeFeed["id"];
@@ -39,7 +39,41 @@ const ZeroLike = styled.span`
   color: silver;
 `;
 
+//ShowLikeUserBox
+const ShowLikeUserBox = styled(defaultFlexBox)`
+  position: fixed;
+  opacity: 0.7;
+  height: 100vh;
+  top: 0;
+  width: 100%;
+  left: 0;
+  background-color: black;
+  z-index: 1;
+`;
+
+const Wrapper = styled.div`
+  border: 1px solid white;
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  color: white;
+  position: relative;
+  padding-top: 20px;
+  z-index: 100;
+`;
+
+const Icon = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+`;
+
 export default function Likes({ photoId, likes }: PhotoPick) {
+  const [showLikesUser, setShowLikesUser] = useState(false);
+  const toggleShowLikeUser = () => setShowLikesUser((prev) => !prev);
   const { data } = useQuery<seePhotoLikes, seePhotoLikesVariables>(
     SEE_PHOTO_LIKES,
     {
@@ -51,9 +85,32 @@ export default function Likes({ photoId, likes }: PhotoPick) {
 
   return (
     <Box>
-      <span>
+      <span onClick={toggleShowLikeUser} style={{ cursor: "pointer" }}>
         좋아요 <NumberColor>{likes}개</NumberColor>
       </span>
+
+      {showLikesUser && (
+        <ShowLikeUserBox>
+          <Wrapper>
+            <Icon onClick={toggleShowLikeUser}>
+              <AiOutlineCloseCircle size={30} />
+            </Icon>
+
+            <div style={{ textAlign: "center" }}>
+              <span style={{ fontSize: 24 }}>Likes</span>
+            </div>
+
+            <div style={{ marginTop: 30, paddingLeft: 20 }}>
+              {data?.seePhotoLikes?.map((item) => (
+                <div style={{ marginBottom: 20 }}>
+                  <Image src={item?.avatar || undefined} />
+                  <span style={{ fontSize: 24 }}>{item?.username}</span>
+                </div>
+              ))}
+            </div>
+          </Wrapper>
+        </ShowLikeUserBox>
+      )}
 
       {data?.seePhotoLikes?.map((like, index) => (
         <LikeUser key={like?.id}>
@@ -92,37 +149,4 @@ export default function Likes({ photoId, likes }: PhotoPick) {
       ))}
     </Box>
   );
-}
-
-{
-  /* <Column>
-<Like_LikeNumber>
-  {likes ? (
-    <span>
-      <NumberColor>좋아요 {likes}개</NumberColor>
-      <span>{data?.seePhotoLikes?.map((v) => v?.username)}</span>
-    </span>
-  ) : (
-    <span> 현재 좋아요가 없습니다</span>
-  )}
-</Like_LikeNumber>
-
-<div>
-  {data?.seePhotoLikes?.map((like, index) => (
-    <div>
-      {likes > 1
-        ? index === 0 && (
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Image src={like?.avatar || undefined} />
-              <span style={{ color: "silver" }}>
-                {like?.username}님 외에 {likes - 1}명이 좋아요를
-                눌렀습니다
-              </span>
-            </div>
-          )
-        : null}
-    </div>
-  ))}
-</div>
-</Column> */
 }
